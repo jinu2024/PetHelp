d9cf9383d8c89d00510677a62875ba02624daddb
Jinu Varghese <76210328+jinu2024@users.noreply.github.com>
2025-06-13 15:24:56 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -98,8 +98,8 @@ const loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax",
+      secure: true,       
+      sameSite: "None"
       maxAge: 24 * 60 * 60 * 1000,
     });
 
